3975ee94cb89e6d1b41024d01896328eff6f1439
Kim Rune Solstad <krs@itverket.no>
2023-07-19 11:53:40 +0200

    Revert "Added null check"
    This reverts commit 5e736eb785b560aea495fe40cc9b72eee1944ad5.

--- LabAnswerAnalyser/GlucoseAnalyser.cs
+++ LabAnswerAnalyser/GlucoseAnalyser.cs
@@ -52,10 +52,6 @@ public class GlucoseAnalyser : IGlucoseAnalyzer
         
         var labAnswers = await _labAnswerService.ByPatientThrowIfNone(patient.Id);
         var labAnswer = labAnswers.First(x => x.ExaminationType == ExaminationType.Glucose);
-        if (labAnswer.GlucoseLevel == null)
-        {
-            throw new NullReferenceException(nameof(labAnswer.GlucoseLevel));
-        }
         
         // We do not have the zodiac sign of patients that registered before we incorporated astrology
         var shouldNotifyPatient = patient.ZodiacSign is not null
